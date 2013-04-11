bf10c8e5af9aa1ec09cfefcae901d5dcbfb2ec1f
Robert Pohl <robban.p@gmail.com>
2013-04-11 16:49:01 +0200

    first

---
+++ AzureIndex/IndexHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureIndex
+{
+
+    public class IndexHandler
+    {
+        public static DateTimeOffset? LastModified { get; set; }
+        public static Object IndexLock = new Object();
+
+        public static void PushToStorage(string indexPath, string packageFileName, string storageInfo, string containerName)
+        {
+            var destDir = System.IO.Directory.GetParent(indexPath).ToString();//index dir parent to store archive
+            var destPath = System.IO.Path.Combine(destDir, packageFileName);
+            StorageHandler.CreateArchive(indexPath,destPath);
+            var storage = new StorageHandler(storageInfo);
+            storage.SetBlob(containerName, destPath);
+        }
+
+        public static void CheckStorage(string storageInfo, string containerName, string packageFileName, string indexPath)
+        {
+            var storage = new StorageHandler(storageInfo);
+            var blob = storage.GetBlob(containerName, packageFileName);
+
+            blob.FetchAttributes();
+            var modified = blob.Properties.LastModified;
+            if (modified > LastModified || LastModified == null)
+            {
+                var zipPath = Directory.GetParent(indexPath).ToString();
+                var archiveDest = Path.Combine(zipPath, packageFileName);
+                using (var fileStream = System.IO.File.OpenWrite(archiveDest))
+                {
+                    blob.DownloadToStream(fileStream);
+                }
+                LastModified = modified;
+                lock (IndexLock)
+                {
+
+                    StorageHandler.ExtractArchive(indexPath + "_tmp", archiveDest);
+                    if (Directory.Exists(indexPath))
+                    {
+                        Directory.Delete(indexPath, true);
+                    }
+                    Directory.Move(indexPath + "_tmp", indexPath);
+                }
+                //unlock
+            }
+        }
+    }
+}
---
+++ AzureIndex/StorageHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureIndex
+{
+
+    public class StorageHandler
+    {
+        private CloudStorageAccount storageAccount { get; set; }
+
+        public StorageHandler(string storageInfo)
+        {
+            storageAccount = CloudStorageAccount.Parse(storageInfo);
+        }
+
+        public CloudBlockBlob GetBlob(string container, string fileName)
+        {
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference(container);
+            return blobContainer.GetBlockBlobReference(fileName);
+        }
+
+        public void SetBlob(string container, string fileName)
+        {
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference(container);
+            blobContainer.CreateIfNotExists();
+            blobContainer.SetPermissions(
+                new BlobContainerPermissions
+                {
+                    PublicAccess =
+                        BlobContainerPublicAccessType.Blob
+                });
+
+            var blockBlob = blobContainer.GetBlockBlobReference(Path.GetFileName(fileName));
+            blockBlob.UploadFromStream(File.OpenRead(fileName));
+        }
+
+        public static void CreateArchive(string inDir, string outFile)
+        {
+            if (File.Exists(outFile))
+            {
+                File.Delete(outFile);
+            }
+            ZipFile.CreateFromDirectory(inDir, outFile);
+        }
+
+        public static void ExtractArchive(string outDir, string inFile)
+        {
+            ZipFile.ExtractToDirectory(inFile, outDir);
+        }
+    }
+}
